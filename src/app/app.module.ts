d99d69a67d15a6c9ee3595d03e7bd9f456b1e9cf
Ovata <ovataworld@gmial.com>
2019-08-06 21:40:09 +0100

    folder structure and  components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './components/index/index.component';
+import { RegistratonComponent } from './components/registraton/registraton.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    RegistratonComponent,
+    LoginComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/registraton/registraton.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registraton',
+  templateUrl: './registraton.component.html',
+  styleUrls: ['./registraton.component.css']
+})
+export class RegistratonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
