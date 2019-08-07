c13d84eda00254e05122cb8657e89421a6680170
Ovata <ovataworld@gmial.com>
2019-08-07 07:41:21 +0100

    Worked  on the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
+import { RegistratonComponent } from './components/registraton/registraton.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { LoginComponent } from './components/login/login.component';
+import { IndexComponent } from './components/index/index.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
